9996b872209bf6f8dbf156711d5eb4af67488b7a
MihaelHoli <mholi@student.unipu.hr>
2024-11-20 21:22:52 +0100

    test 4

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,13 @@
+const isProd = process.env.NODE_ENV === 'production';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: '',
-  assetPrefix: '',
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? '/MihaelHoli.github.io/' : '',
+  basePath: isProd ? '/MihaelHoli.github.io' : '',
   output: 'export',
   distDir: 'out'
 };
