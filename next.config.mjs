70a3b5ed9ce8d393b8b44463d452006e2aea2816
MihaelHoli <mholi@student.unipu.hr>
2024-11-20 22:02:43 +0100

    hopefully last update

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: "/MihaelHoli.github.io",
+  assetPrefix: "/MihaelHoli.github.io/",
   output: "export",
   reactStrictMode: true
 };
